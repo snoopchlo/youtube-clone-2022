b063d368eeef5172a4a82e487ec76457d230e414
Chloe Lee <chloelee00516@gmail.com>
2022-08-12 17:36:40 +0900

    pug mixins
    pug mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -19,5 +19,33 @@ export const deleteVideo = (req, res) => {
 //   res.send("delete commentt on Videos");
 
 export const search = (req, res) => res.send("Search Video");
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser });
+export const trending = (req, res) => {
+  const fakeVideos = [
+    {
+      title: "video 1",
+      rating: 4,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "video 2",
+      rating: 4,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "video 3",
+      rating: 4,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+  ];
+
+  return res.render("home", { pageTitle: "Home", fakeUser, fakeVideos });
+};
