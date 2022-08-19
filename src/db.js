4503ce8773902c93a0ddbcec1577da620e1a3ac4
Chloe Lee <chloelee00516@gmail.com>
2022-08-19 17:39:27 +0900

    added model
    stupid typo!!!!!!!!!

---
+++ src/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://127.0.0.1:27017/Utube", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB");
+const handleError = (error) => console.log("🆘 DB Error", error);
+
+db.on("error", handleError);
+db.once("open", handleOpen);
