1825255b8597a09ed842ac265468f4f612a283b3
Chloe Lee <chloelee00516@gmail.com>
2022-08-21 17:55:41 +0900

    upload,edit
    upload,edit

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -12,19 +12,30 @@ export const home = (req, res) => {
 export const home = async (req, res) => {
   const videos = await Video.find({});
   Video.find({}, (error, videos) => {});
+  console.log(videos);
   return res.render("home", { pageTitle: "Home", videos });
 };
 
-export const watchVideo = (req, res) => {
+export const watchVideo = async (req, res) => {
   const { id } = req.params;
-  return res.render("watch", {
-    pageTitle: "Watch",
-  });
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video Not Found" });
+  } else {
+    return res.render("watch", {
+      pageTitle: video.title,
+      video,
+    });
+  }
 };
 
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-  return res.render("edit", { pageTitle: "Edit" });
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video Not Found" });
+  }
+  return res.render("edit", { pageTitle: `Edit ${video.title}`, video });
 };
 export const postEdit = (req, res) => {
   const { id } = req.params;
@@ -46,7 +57,21 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 
-export const postUpload = (req, res) => {
-  const { title } = req.body;
-  return res.redirect("/");
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  try {
+    await Video.create({
+      title,
+      description,
+      hashtags: hashtags.split(",").map((word) => `#${word}`),
+    });
+
+    return res.redirect("/");
+  } catch (error) {
+    console.log(error);
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
 };
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -10,9 +10,9 @@ import {
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", watchVideo);
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.get("/:id([0-9a-f]{24})", watchVideo);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.get("/:id/delete", deleteVideo);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 // videoRouter.get("/comment", commentVideo);
