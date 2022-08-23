943842eb9eb90bbcc5e0b018882198b105198ca1
Chloe Lee <chloelee00516@gmail.com>
2022-08-24 00:05:11 +0900

    hash passwords
    with using bcrypt. never save password in your db. must be hashed

--- src/models/User.js
+++ src/models/User.js
@@ -3,15 +3,22 @@
 // [3] export User model
 
 import mongoose from "mongoose";
-// const { Schema } = mongoose;
+import bcrypt from "bcrypt";
+const { Schema } = mongoose;
 
 // Everything in Mongoose starts with a schema (maps to a MongoDB and define the shape of the document)
-const userSchema = new mongoose.Schema({
+const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  location: { type: String, required: true },
+  location: String,
+});
+
+userSchema.pre("save", async function () {
+  console.log("password before hashing: ", this.password);
+  this.password = await bcrypt.hash(this.password, 5);
+  console.log("password after hashing: ", this.password);
 });
 
 // To use our schema definition, need to convert the schema into a model
